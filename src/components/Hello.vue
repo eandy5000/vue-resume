8c70ad0cae22af5f022318f88c0ee51a4a9455a9
Andy Eccleston <eandy5000@yahoo.com>
2017-05-06 15:17:26 -0500

    added vuex store

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <h2>Essential Links {{ store }}</h2>
     <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
@@ -26,6 +26,11 @@ export default {
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  computed: {
+    store () {
+      return this.$store.state.test
     }
   }
 }
