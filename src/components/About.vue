d7f05f75f2be32b3f7ea366fa477c5877a53938b
Andy Eccleston <eandy5000@yahoo.com>
2017-06-13 19:02:05 -0500

    added PostCSS oldie and cssnext to the project and added text about postcss to about component

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,13 +1,13 @@
 <template>
-	<div>
+	<div id="fragile">
 	<h2>About This Website</h2>
-	<p>The best way to view this page is with a Google Chrome browser.</p>
+	<p>The best way to view this page is built and tested with a Google Chrome browser.</p>
 	<strong>Front End Javascript</strong>
 	<p>This website is a Single Page Application, that uses ES6 Javascript with Babel and Vue JS. It uses Vue-Router for page routing and Vuex for managing state.</p>
 	<strong>Styles</strong>
-	<p>The website uses custom CSS styles with Flexbox to make a responsive design for desktop, tablet and mobile users. It also uses Normalize CSS to improve cross browser experience and Vue's CSS scoping feature to make components with modular styling.</p>
+	<p>The website uses custom CSS styles with Flexbox to make a responsive design for desktop, tablet and mobile users. It also uses PostCSS and Normalize CSS to improve cross browser experience and Vue's CSS scoping feature to make components with modular styling.</p>
 	<strong>Build and Deploy</strong>
-	<p>I used <a href="https://github.com/vuejs/vue-cli">Vue Cli</a> to generate the boilerplate for this project. I used Node JS with express to serve the site and <a href="https://surge.sh/">Surge</a> to deploy the site.</p>
+	<p>I used <a href="https://github.com/vuejs/vue-cli">Vue Cli</a> to generate the boilerplate for this project. I used Node JS with the Express framework to serve the site and <a href="https://surge.sh/">Surge</a> to deploy the site.</p>
 	</div>
 </template>
 
